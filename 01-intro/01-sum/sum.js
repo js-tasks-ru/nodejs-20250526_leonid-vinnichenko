1a6de874df27e448d96f8337f2d76290f6f4926f
lvinnichenko <lvinnichenko@aligntech.com>
2025-06-02 17:21:07 +0200

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,3 +1,7 @@
 export default function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new TypeError("Both arguments must be numbers");
+  }
+
+  return a + b;
 }
