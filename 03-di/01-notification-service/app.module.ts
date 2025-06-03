be0f6c762d187619d0f3c57c4b327effee914e30
vinneyto <vinneyto@gmail.com>
2025-06-03 23:17:14 +0200

    added solution for 3-module 1-task - notification service

--- 03-di/01-notification-service/app.module.ts
+++ 03-di/01-notification-service/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
+import { UsersModule } from "./users/users.module";
+import { NotificationsModule } from "./notifications/notifications.module";
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, UsersModule, NotificationsModule],
 })
 export class AppModule {}
